7a5991f6ec14808733c6912f0ab10598d16ad39d
Valentin <va.kog@aon.at>
2021-11-25 09:48:07 +0100

    added extends Gameview in ImageController

--- programs/memory_v0-1/src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/ImageController.java
+++ programs/memory_v0-1/src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/ImageController.java
@@ -9,13 +9,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-public class ImageController {
+public class ImageController extends Gameview{
 
     public static AnchorPane setImage(int i, AnchorPane holder) throws FileNotFoundException {
 
         String path;
-        if(Gameview.cards[i].isPictureShown()){
-            path = "src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/pics/theme0/image" + Gameview.cards[i].getPictureId() + ".png";
+        if(cards[i].isPictureShown()){
+            path = "src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/pics/theme0/image" + cards[i].getPictureId() + ".png";
         }
         else {
             path = "src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/pics/cover/cover0.jpg";
@@ -23,13 +23,13 @@ public class ImageController {
         File directory = new File(path);
         Image image = new Image(new FileInputStream(directory.getAbsoluteFile()));
         ImageView imageView = new ImageView(image);
-        int x = Gameview.spaceX + Gameview.cards[i].getPosition()[0] * (Gameview.size + Gameview.spaceX);
-        int y = Gameview.spaceY + Gameview.cards[i].getPosition()[1] * (Gameview.size + Gameview.spaceY) + Gameview.gameheaderSize;
+        int x = spaceX + cards[i].getPosition()[0] * (size + spaceX);
+        int y = spaceY + cards[i].getPosition()[1] * (size + spaceY) + gameheaderSize;
         imageView.setX(x);
         imageView.setY(y);
         //imageView.setTranslateZ(20);
-        Gameview.cards[i].setPixel(x, y, x+Gameview.size, y+Gameview.size);
-        imageView.setFitWidth(Gameview.size);
+        cards[i].setPixel(x, y, x+size, y+size);
+        imageView.setFitWidth(size);
         imageView.setPreserveRatio(true);
         holder.getChildren().add(imageView);
         return holder;
