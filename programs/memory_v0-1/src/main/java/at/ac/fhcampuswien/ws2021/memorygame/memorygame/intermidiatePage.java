e307b0c8261e61237a00431829b29e0ebd5da889
Cindy <maximilian.hartl@stud.fh-campuswien.ac.at>
2021-12-29 20:22:01 +0100

    first steps

--- programs/memory_v0-1/src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/intermidiatePage.java
+++ programs/memory_v0-1/src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/intermidiatePage.java
@@ -1,4 +1,32 @@
 package at.ac.fhcampuswien.ws2021.memorygame.memorygame;
 
-public class intermidiatePage {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+import java.awt.*;
+
+public class intermidiatePage extends Application {
+    Button button;
+    public static void main(String[] args) {
+            launch(args);
+
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Intermediate Page");
+        button=new Button();
+        button.setText("click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 1600, 900);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
