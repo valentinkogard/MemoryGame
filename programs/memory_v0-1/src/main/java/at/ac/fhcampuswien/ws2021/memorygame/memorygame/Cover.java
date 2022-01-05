38fc9c30add047f90c610daf5ab7baaa5088e493
Ehud321 <nikolaus.kapfer-rupp@stud.fh-campuswien.ac.at>
2022-01-05 11:06:25 +0100

    Changed Titel to Memorycards; currently not working

--- programs/memory_v0-1/src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/Cover.java
+++ programs/memory_v0-1/src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/Cover.java
@@ -37,12 +37,14 @@ public class Cover extends Application {
         primaryStage.setTitle("MEMORY");
         BorderPane border = new BorderPane();
 
-        VBox vbox = new VBox ();
+        VBox vbox = new VBox();
         VBox vbox1 = new VBox();
         VBox vbox2 = new VBox();
+        HBox hbox = new HBox();
 
 
         border.setCenter(vbox);
+        border.setTop(hbox);
 
 
         Scene mainpageScene = new Scene(border, windowSize[0], windowSize[1]);
@@ -51,18 +53,101 @@ public class Cover extends Application {
 
         primaryStage.setScene(mainpageScene);
 
+        String path1 = "src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/pics/cover/cover1.png";
+        File directory1 = new File(path1);
+        Image image1 = new Image(new FileInputStream(directory1.getAbsoluteFile()));
+        ImageView card1 = new ImageView(image1);
+        ImageView card2 = new ImageView(image1);
+        ImageView card3 = new ImageView(image1);
+        ImageView card4 = new ImageView(image1);
+        ImageView card5 = new ImageView(image1);
+        ImageView card6 = new ImageView(image1);
+        card1.setFitHeight(100);
+        card1.setPreserveRatio(true);
+        card2.setFitHeight(100);
+        card2.setPreserveRatio(true);
+        card3.setFitHeight(100);
+        card3.setPreserveRatio(true);
+        card4.setFitHeight(100);
+        card4.setPreserveRatio(true);
+        card5.setFitHeight(100);
+        card5.setPreserveRatio(true);
+        card6.setFitHeight(100);
+        card6.setPreserveRatio(true);
+
+
+
+
+
+        Button m = new Button();
+        m.setGraphic(card1);
+        m.setStyle("-fx-background-color: transparent; -fx-text-fill: white;");
+        m.setFont(Font.font("Calibri", 25));
+        m.setText("");
+        m.setPrefSize(10,10);
+
+
+
+        Button e1 = new Button();
+        e1.setGraphic(card2);
+        e1.setStyle("-fx-background-color: transparent; -fx-text-fill: white;");
+        e1.setFont(Font.font("Calibri", 25));
+        e1.setText("");
+        e1.setPrefSize(10,10);
+
+        Button m1 = new Button();
+        m1.setGraphic(card3);
+        m1.setStyle("-fx-background-color: transparent; -fx-text-fill: white;");
+        m1.setFont(Font.font("Calibri", 25));
+        m1.setText("");
+        m1.setPrefSize(10,10);
+
+        Button o = new Button();
+        o.setGraphic(card4);
+        o.setStyle("-fx-background-color: transparent; -fx-text-fill: white;");
+        o.setFont(Font.font("Calibri", 25));
+        o.setText("");
+        o.setPrefSize(10,10);
+
+        Button r = new Button();
+        r.setGraphic(card5);
+        r.setStyle("-fx-background-color: transparent; -fx-text-fill: white;");
+        r.setFont(Font.font("Calibri", 25));
+        r.setText("");
+        r.setPrefSize(10,10);
+
+        Button y = new Button();
+        y.setGraphic(card6);
+        y.setStyle("-fx-background-color: transparent; -fx-text-fill: white;");
+        y.setFont(Font.font("Calibri", 25));
+        y.setText("");
+        y.setPrefSize(10,10);
+
+
+
+
+
+        hbox.setAlignment(Pos.CENTER);
+        hbox.getChildren().addAll(m,e1,m1,o,r,y);
+
+
+
+
+
+
+
 
 
         //-------------------------Title & Background-------------------------
-        Text title = new Text(10, 50, "MEMORY");
-        title.setFont(Font.font("Calibri", FontWeight.EXTRA_BOLD, 80));
-        title.setFill(Color.ORANGERED);
+        //Text title = new Text(10, 50, "MEMORY");
+        //title.setFont(Font.font("Calibri", FontWeight.EXTRA_BOLD, 80));
+        //title.setFill(Color.ORANGERED);
 
 
 
         //vbox.setBackground(new Background(new BackgroundFill(Color.BLANCHEDALMOND, new CornerRadii(0), Insets.EMPTY)));
 
-        String path0 = "src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/pics/questionmark.png";
+        String path0 = "src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/pics/background/background5.png";
         File directory0 = new File(path0);
         //FileInputStream inp = new FileInputStream("c:\\Users\\nikol\\Desktop\\pic\\background5.png");
         Image bgImage = new Image(new FileInputStream(directory0.getAbsoluteFile()));
@@ -77,12 +162,12 @@ public class Cover extends Application {
 
         border.setBackground(bg);
 
-        String path1 = "src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/pics/questionmark.png";
-        File directory1 = new File(path1);
+        String path4 = "src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/pics/background/background6.png";
+        File directory4 = new File(path4);
         //FileInputStream inp1 = new FileInputStream("c:\\Users\\nikol\\Desktop\\pic\\background6.png");
-        Image bgImage1 = new Image(new FileInputStream(directory1.getAbsoluteFile()));
+        Image bgImage4 = new Image(new FileInputStream(directory4.getAbsoluteFile()));
 
-        BackgroundImage bi1 = new BackgroundImage(bgImage1, BackgroundRepeat.NO_REPEAT,
+        BackgroundImage bi1 = new BackgroundImage(bgImage4, BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
@@ -90,7 +175,33 @@ public class Cover extends Application {
 
         Background bg1 = new Background(bi1);
 
-        vbox.setBackground(bg);
+        String path5 = "src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/pics/background/catLove2.gif";
+        //FileInputStream inp2 = new FileInputStream("c:\\Users\\nikol\\Desktop\\pic\\catLove2.gif");
+        Image bgImage2 = new Image(path5);
+
+        BackgroundImage bi2 = new BackgroundImage(bgImage2, BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+
+
+        Background bg2 = new Background(bi2);
+
+        String path6 = "src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/pics/background/background4.png";
+        //FileInputStream inp3 = new FileInputStream("c:\\Users\\nikol\\Desktop\\pic\\background4.png");
+        Image bgImage3 = new Image(path6);
+
+        BackgroundImage bi3 = new BackgroundImage(bgImage3, BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+
+
+        Background bg3 = new Background(bi3);
+
+
+
+
 
 
         //-------------------------Singleplayer Button-------------------------
@@ -152,6 +263,7 @@ public class Cover extends Application {
             public void handle(MouseEvent mouseEvent) {
 
                 multiPlayer.setStyle("-fx-background-color: linear-gradient(to bottom, darkslateblue, violet); -fx-text-fill: white;");
+                border.setBackground(bg2);
 
             }
         });
@@ -161,7 +273,7 @@ public class Cover extends Application {
             public void handle(MouseEvent mouseEvent) {
 
                 multiPlayer.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
-
+                border.setBackground(bg);
             }
         });
 
@@ -196,7 +308,7 @@ public class Cover extends Application {
             public void handle(MouseEvent mouseEvent) {
 
                 options.setStyle("-fx-background-color: linear-gradient(to bottom, darkslateblue, violet); -fx-text-fill: white;");
-
+                border.setBackground(bg3);
             }
         });
 
@@ -205,6 +317,7 @@ public class Cover extends Application {
             public void handle(MouseEvent mouseEvent) {
 
                 options.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
+                border.setBackground(bg);
 
             }
         });
@@ -298,7 +411,7 @@ public class Cover extends Application {
             @Override
             public void handle(MouseEvent mouseEvent) {
 
-                vbox.setBackground(bg1);
+                border.setBackground(bg1);
                 exit.setStyle("-fx-background-color: linear-gradient(to bottom, darkslateblue, violet); -fx-text-fill: white;");
 
             }
@@ -308,7 +421,7 @@ public class Cover extends Application {
             @Override
             public void handle(MouseEvent mouseEvent) {
 
-                vbox.setBackground(bg);
+                border.setBackground(bg);
                 exit.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
             }
         });
@@ -333,20 +446,29 @@ public class Cover extends Application {
 
         singlePlayer.setMinWidth(200);
         singlePlayer.setMinHeight(40);
+        singlePlayer.setCursor(Cursor.HAND);
 
         multiPlayer.setMinWidth(200);
         multiPlayer.setMinHeight(40);
+        multiPlayer.setCursor(Cursor.HAND);
 
         options.setMinWidth(200);
         options.setMinHeight(40);
+        options.setCursor(Cursor.HAND);
 
         credits.setMinWidth(200);
         credits.setMinHeight(40);
+        credits.setCursor(Cursor.HAND);
 
         exit.setMinWidth(200);
         exit.setMinHeight(40);
+        exit.setCursor(Cursor.HAND);
 
         //-------------------------VBox Button Width & Spacing  Setting-------------------------
+
+        hbox.setPrefWidth(50);
+        hbox.setSpacing(5);
+
 
         vbox.setPrefWidth(200);
         vbox.setSpacing(50);
@@ -367,7 +489,7 @@ public class Cover extends Application {
         vbox.setAlignment(Pos.CENTER);
         credVbox.setAlignment(Pos.CENTER);
 
-        vbox.getChildren().addAll(title, singlePlayer, multiPlayer, options, credits, exit);
+        vbox.getChildren().addAll( singlePlayer, multiPlayer, options, credits, exit);
 
         vbox1.getChildren().add(exit1);
 
