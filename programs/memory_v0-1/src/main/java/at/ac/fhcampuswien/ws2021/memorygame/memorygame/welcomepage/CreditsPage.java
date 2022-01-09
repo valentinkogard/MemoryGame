f9b484d60af662fc297584e1e94b236ce9d141b2
ifrim <christina.ifrim@stud.fh-campus.ac.at>
2022-01-09 21:43:00 +0100

    new background for credit page

--- programs/memory_v0-1/src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/welcomepage/CreditsPage.java
+++ programs/memory_v0-1/src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/welcomepage/CreditsPage.java
@@ -4,25 +4,42 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class CreditsPage {
 
     public static final int[] windowSize = {1000, 600};
 
 
-    public Object [] CreditsPageInit() {
+    public Object [] CreditsPageInit() throws FileNotFoundException {
 
         VBox credVbox = new VBox();
         Scene credScene = new Scene(credVbox, windowSize[0], windowSize[1]);
-        credVbox.setBackground(new Background(new BackgroundFill(Color.BLANCHEDALMOND, new CornerRadii(0), Insets.EMPTY)));
+
+        String path1 = "src/main/java/at/ac/fhcampuswien/ws2021/memorygame/memorygame/pics/background/old-black-background.jpg.jpg";
+        File directory1 = new File(path1);
+        Image bgImage1 = new Image(new FileInputStream(directory1.getAbsoluteFile()));
+
+        BackgroundImage bi = new BackgroundImage(bgImage1, BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+
+
+        javafx.scene.layout.Background bg = new javafx.scene.layout.Background(bi);
+
+
+        credVbox.setBackground(bg);
 
 
         Text title1 = new Text("CREDITS");
